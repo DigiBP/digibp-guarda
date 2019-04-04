67e9a92328f73a0baa3dc3fd97c63224ab40b535
Hermann Grieder <hermann.grieder@students.fhnw.ch>
2019-04-04 13:27:43 +0200

    addeed cors configuration

--- src/main/java/ch/fhnw/digibp/ProcessApplication.java
+++ src/main/java/ch/fhnw/digibp/ProcessApplication.java
@@ -3,6 +3,12 @@ package ch.fhnw.digibp;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 
 @SpringBootApplication
 @EnableProcessApplication
@@ -12,4 +18,18 @@ public class ProcessApplication {
 		SpringApplication.run(ProcessApplication.class, args);
 	}
 
+    @Bean
+    public FilterRegistrationBean processCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
 }
